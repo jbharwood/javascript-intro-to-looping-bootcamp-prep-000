19b4eb9809a8275397f93eb30b6c02ecd2c7bf4d
Joseph Harwood <josephblakeharwood@gmail.com>
2018-08-21 01:27:31 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,7 +2,7 @@ function forLoop(array) {
   for (let i = 0; i < 26; i++) {
      if (i === 0)
       console.log("I am 1 strange loop.")
-     if ( i > 0)
+     if ( i > 1)
       console.log("I am " + i + " strange loops.")
   }
 }
