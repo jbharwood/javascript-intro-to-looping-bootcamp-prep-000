c9b49488f4bf4636f3c8a9783a08f7360451aa1c
Joseph Harwood <josephblakeharwood@gmail.com>
2018-08-21 01:53:45 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -16,6 +16,14 @@ function whileLoop(n) {
   return "done"
 }
 
+function incrementVariable() {
+  i = i + 1;
+}
+
 function doWhileLoop(array) {
-  
+  do {
+    incrementVariable()
+  } while {
+    
+  }
 }
